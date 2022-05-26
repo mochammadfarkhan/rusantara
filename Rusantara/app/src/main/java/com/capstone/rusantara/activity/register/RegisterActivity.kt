2631fcfdc97497b298993e986b4ddf11dc9f014d
Raden <radenwisnu21@gmail.com>
2022-05-26 15:54:21 +0700

    update register

--- Rusantara/app/src/main/java/com/capstone/rusantara/activity/register/RegisterActivity.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/activity/register/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.capstone.rusantara.activity.register
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.capstone.rusantara.R
@@ -19,6 +22,7 @@ class RegisterActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        playAnimation()
     }
 
     private fun setupView() {
@@ -42,9 +46,50 @@ class RegisterActivity : AppCompatActivity() {
 
             when {
                 username.isEmpty() -> {
-                    binding.idUsername.error = ""
+                    binding.idUsername.error = R.string.error_username.toString()
+                }
+                email.isEmpty() -> {
+                    binding.idEmail.error = R.string.error_email.toString()
+                }
+                password.isEmpty() -> {
+                    binding.idPassword.error = R.string.error_password.toString()
+                }
+                else -> {
+
                 }
             }
         }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.logo, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val title = ObjectAnimator.ofFloat(binding.register, View.ALPHA, 1f).setDuration(500)
+        val usernameTextView = ObjectAnimator.ofFloat(binding.usernameTextView, View.ALPHA, 1f).setDuration(500)
+        val usernameEditTextLayout = ObjectAnimator.ofFloat(binding.tvUsername, View.ALPHA, 1f).setDuration(500)
+        val emailTextView = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(500)
+        val emailEditTextLayout = ObjectAnimator.ofFloat(binding.tvEmail, View.ALPHA, 1f).setDuration(500)
+        val passwordTextView = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(500)
+        val passwordEditTextLayout = ObjectAnimator.ofFloat(binding.tvPassword, View.ALPHA, 1f).setDuration(500)
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(500)
+
+
+        AnimatorSet().apply {
+            playSequentially(
+                title,
+                usernameTextView,
+                usernameEditTextLayout,
+                emailTextView,
+                emailEditTextLayout,
+                passwordTextView,
+                passwordEditTextLayout,
+                signup
+            )
+            startDelay = 500
+        }.start()
     }
 }
