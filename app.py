f86f56de0211913f68d8a9692797e53999c89b0e
seika333 <fpsdragon333@gmail.com>
2022-05-31 08:43:54 +0700

    add some classes and updating the ml model

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import sys
 model = load_model('./model/rusantara_model.h5')
 
 def predict():
-  img_path = 'uploads/' + sys.argv[1];
+  img_path = './images/resep-onde-onde.jpg'
   img = image.load_img(img_path, target_size=(160, 160))
   img = image.img_to_array(img)
   img = np.expand_dims(img, axis=0)
-  img = np.vstack([img]) 
+  img = np.vstack([img])
 
   result = model.predict(img)
 
-  foods = ['dadar gulung', 'lapis legit', 'onde-onde']
+  foods = ['clorot', 'dadar gulung', 'klepon', 'lapis legit', 'onde-onde', 'putu ayu']
 
-  prediction = foods[np.argmax(result)]
+  prediction = foods[np.argmax(result[0])]
 
   print(prediction)
 
