ef9bce8473d489009258da24ed749b8431a720c9
Raden <radenwisnu21@gmail.com>
2022-05-31 17:23:21 +0700

    update profile fragment

--- Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/profile/ProfileFragment.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/profile/ProfileFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.capstone.rusantara.databinding.FragmentNotificationsBinding
+import com.capstone.rusantara.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class ProfileFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.profUsernameValue
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
