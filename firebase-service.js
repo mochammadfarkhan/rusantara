32b5045c653ea7873514c41c71a248157efa3beb
seika333 <fpsdragon333@gmail.com>
2022-05-31 08:42:25 +0700

    move files to src folder

--- firebase-service.js
+++
@@ -1,8 +0,0 @@
-const serviceAccount = require('./serviceAccountKey.json');
-
-const admin = require('firebase-admin');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-
-module.exports = admin;
