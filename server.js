e7ff2e181d06b12a3f313884b02296fffc1a3779
seika333 <fpsdragon333@gmail.com>
2022-06-01 23:55:57 +0700

    change port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post(
   }
 );
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
   console.log(`listening on port ${PORT}`);
 });
