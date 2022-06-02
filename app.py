36185c44a7e508b725849814f7e3ae8b24c6ac43
seika333 <fpsdragon333@gmail.com>
2022-06-02 21:05:28 +0700

    change import syntax for keras utils

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 import keras
 from keras.models import load_model
-from keras.preprocessing import image
+from tensorflow.keras.utils import img_to_array, load_img
 import numpy as np
 import sys
 
 model = load_model('./model/rusantara_model.h5')
 
 def predict():
-  img_path = './images/resep-onde-onde.jpg'
-  img = image.load_img(img_path, target_size=(160, 160))
-  img = image.img_to_array(img)
+  img_path = 'uploads/' + sys.argv[1]
+  img = load_img(img_path, target_size=(160, 160))
+  img = img_to_array(img)
   img = np.expand_dims(img, axis=0)
   img = np.vstack([img])
 
-  result = model.predict(img)
+  result = model.predict(img, verbose=0)
 
   foods = ['clorot', 'dadar gulung', 'klepon', 'lapis legit', 'onde-onde', 'putu ayu']
 
