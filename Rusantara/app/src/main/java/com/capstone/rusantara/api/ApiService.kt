6f288854fa22d99262a95c7e712ca3f998c9ba08
Raden <radenwisnu21@gmail.com>
2022-06-05 20:18:35 +0700

    add firebase authentication, update apiservice

--- Rusantara/app/src/main/java/com/capstone/rusantara/api/ApiService.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/api/ApiService.kt
@@ -1,5 +1,18 @@
 package com.capstone.rusantara.api
 
+import com.capstone.rusantara.models.SignupResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.POST
+
 interface ApiService {
+    @POST("signup")
+    fun signupUser(
+        @Field("username") username: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<SignupResponse>
+
 
 }
---
+++ Rusantara/app/src/main/java/com/capstone/rusantara/models/SignupResponse.kt
@@ -0,0 +1,8 @@
+package com.capstone.rusantara.models
+
+data class SignupResponse(
+    val uid: String?,
+    val email: String?,
+    val displatName: String?,
+    val error: String?
+)
