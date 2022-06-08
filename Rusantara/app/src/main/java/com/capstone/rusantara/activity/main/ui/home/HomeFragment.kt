f4c144d50786b5229a3f15595da5a2aa0ac47608
Raden <radenwisnu21@gmail.com>
2022-06-08 15:46:59 +0700

    update menu

--- Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/home/HomeFragment.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.capstone.rusantara.activity.main.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.capstone.rusantara.activity.upload.UploadImageActivity
 import com.capstone.rusantara.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,7 +25,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -33,6 +35,15 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener{
+            val intent = Intent(requireContext(), UploadImageActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
--- Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/home/HomeViewModel.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/activity/main/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Introdution"
+        value = "take a photo, upload, and the info will come out"
     }
     val text: LiveData<String> = _text
 }
