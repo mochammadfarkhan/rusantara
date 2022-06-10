84f2727d42c476df06a84a888cb9b3f34c6cb05c
Raden <radenwisnu21@gmail.com>
2022-06-11 02:06:58 +0700

    update ImageData.kt

--- Rusantara/app/src/main/java/com/capstone/rusantara/models/ImageData.kt
+++ Rusantara/app/src/main/java/com/capstone/rusantara/models/ImageData.kt
@@ -1,14 +1,26 @@
 package com.capstone.rusantara.models
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class ImageData(
+    @field:SerializedName("img")
     val img: String,
+
+    @field:SerializedName("name")
     val name: String,
+
+    @field:SerializedName("info")
     val info: String,
+
+    @field:SerializedName("location")
     val location: String,
+
+    @field:SerializedName("ingredients")
     val ingredients: String,
+
+    @field:SerializedName("nutrition")
     val nutrition: String
 ) : Parcelable
